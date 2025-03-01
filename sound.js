29eb37aba40355ed77520740170da8b9c5d8a578
dpwls <dpwls02142@gmail.com>
2025-03-01 10:27:22 +0900

    feat: bgm 수정

--- sound.js
+++ sound.js
@@ -1,17 +1,28 @@
 const dropSound = new Audio('./sound/remove.mp3');
 const breakSound = new Audio('./sound/break.mp3');
-const backgroundSound = new Audio('./sound/supertank.mp3');
+const backgroundSound = new Audio('./sound/MusMus-BGM-128.mp3');
 
-backgroundSound.loop = true;
-backgroundSound.volume = 0.2;
+backgroundSound.volume = 0.1;
+backgroundSound.loop = false;
+
+const loopStart = 0;
+const loopEnd = 28.2;
 
 export function playBGM() {
     backgroundSound.currentTime = 0;
     backgroundSound.play();
+    
+    backgroundSound.addEventListener('timeupdate', () => {
+        if (backgroundSound.currentTime >= loopEnd) {
+            backgroundSound.currentTime = loopStart;
+            backgroundSound.play();
+        }
+    });
 }
 
 export function stopBGM() {
     backgroundSound.pause();
+    backgroundSound.currentTime = 0;
 }
 
 export function playDrop() {
